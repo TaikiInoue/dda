d6c94514fcf7dcd67c96a3d0664437aca5c12be1
TaikiInoue <taikiinoue45@gmail.com>
2020-12-03 09:20:21 +0000

    Add docker/

---
+++ docker/entrypoint.sh
@@ -0,0 +1,10 @@
+cp /tmp/.ssh /root/.ssh -r
+cp /tmp/.zshrc /root/.zshrc
+cp /tmp/.tmux.conf /root/.tmux.conf
+sed -i "s/console, file/file/" /opt/conda/lib/python3.6/site-packages/hydra/conf/hydra/job_logging/default.yaml
+sed -i "s/%F{red}%M@%n%f/%F{blue}%M@%n%f/" /root/.zshrc
+mkdir -p /dgx/github
+mkdir -p /dgx/shared/momo
+sshfs inoue@dgx-momo.office.nablas.com:/dgx/inoue/github/ /dgx/github
+sshfs inoue@dgx-momo.office.nablas.com:/dgx/shared/ /dgx/shared/momo
+/usr/bin/zsh
